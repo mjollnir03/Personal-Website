117e0838cb8e328f6ec860d4e4baece01173dac5
Ellmaer <ellmaer2003@gmail.com>
2023-12-22 12:28:26 -0800

    work in progress, finished util class for the button

--- main-directory/src/main.js
+++ main-directory/src/main.js
@@ -1,4 +1,5 @@
 import '../styles/modern-normalize.css';
 import '../styles/style.css';
-import '../styles//fonts/general-sans/css/general-sans.css';
+import '../styles/fonts/general-sans/css/general-sans.css';
+import '../styles/utils.css';
 
