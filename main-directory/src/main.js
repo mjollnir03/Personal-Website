eaf30d0c1f22b42f27fd3ea53775250bd1c1da43
Ellmaer <ellmaer2003@gmail.com>
2023-12-27 19:43:11 -0800

    Need to finish the "Hamburger"

--- main-directory/src/main.js
+++ main-directory/src/main.js
@@ -1,5 +1,6 @@
 import '../styles/modern-normalize.css';
 import '../styles/style.css';
 import '../styles/fonts/general-sans/css/general-sans.css';
+import '../styles/components/header.css';
 import '../styles/utils.css';
 
